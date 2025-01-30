dd66c708191b23846de78e46331ccbbb25779a79
Y <AH05071@ad.wellpoint.com>
2025-01-30 08:01:44 +0530

    sixth

--- puppy.go
+++ puppy.go
@@ -1,21 +1,12 @@
 package puppy
-import (
-	dog "github.com/palmertt/golang.pkg.dog"
-)
 
-func Bark() string{
+
+func Bark() string {
 	return "Woof"
 }
 
-func Barks() string{
+func Barks() string {
 	return "Woof! Woof! Woof!"
 }
 
-
-func BigBark() string{
-	return dog.WhenGrowUP(Bark())
-} 
-
-func BigBarks() string{
-	return dog.WhenGrowUP(Barks())
 }
